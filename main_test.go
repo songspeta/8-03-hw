7a7a3aeeaa17e5fc93d74fb03224159cc3921dae
songspeta <songpeta@mail.ru>
2025-11-07 11:50:28 +0300

    update .go

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello world!"
+	want := "Hello world!!!"
 	got := hello("world")
 	if got != want {
 		t.Errorf("want: %s got: %s", want, got)
